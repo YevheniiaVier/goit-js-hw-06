d5f3b113bfbf7498c76e856db65ea29068a09e3a
YevheniiaVier <gotashk1983@gmail.com>
2022-10-09 23:30:47 +0300

    adds task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatoes', 'Herbs', 'Condiments'];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const makeIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const ingredientRef = document.createElement('li');
+    ingredientRef.classList.add('item');
+    ingredientRef.textContent = ingredient;
+    return ingredientRef;
+  });
+};
+
+const ingredientElements = makeIngredients(ingredients);
+ingredientsListRef.append(...ingredientElements);
