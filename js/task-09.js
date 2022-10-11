74fde68d52cad84f85394107fe9e316773846cf3
YevheniiaVier <gotashk1983@gmail.com>
2022-10-11 21:20:45 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyRef = document.querySelector('body');
+const colorTextRef = document.querySelector('.color');
+const colorChangeBtnRef = document.querySelector('button.change-color');
+
+colorChangeBtnRef.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorTextRef.textContent = bodyRef.style.backgroundColor;
 }
