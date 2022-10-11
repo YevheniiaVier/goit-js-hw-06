77799e8ba5c69c7ad65f7156b97488987196eb59
YevheniiaVier <gotashk1983@gmail.com>
2022-10-11 17:02:05 +0300

    task 8

--- js/task-06.js
+++ js/task-06.js
@@ -2,16 +2,6 @@ const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', onInputBlur);
 
-// function onInputBlur(event) {
-//   if (event.currentTarget.value.length < event.currentTarget.dataset.length) {
-//     event.currentTarget.classList.add('invalid');
-//     event.currentTarget.classList.remove('valid');
-//   } else {
-//     event.currentTarget.classList.remove('invalid');
-//     event.currentTarget.classList.add('valid');
-//   }
-// }
-
 function onInputBlur(event) {
   event.currentTarget.classList.add('valid') ||
     event.currentTarget.classList.replace('invalid', 'valid');
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,35 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  for (const value of Object.values(formData)) {
+    if (!value) {
+      alert('Please fill all fields');
+      return;
+    }
+  }
+  console.log(formData);
+  formRef.reset();
+}
+
+//   const formData = new FormData(event.currentTarget);
+
+//   const formProps = Object.fromEntries(formData); // створюємо реальний обєкт з даними форми, щоб була можливість перервати ітерацію по ньому і не виводити алерт двічі, якщо обидва інпута пусті
+
+//   for (const value of Object.values(formProps)) {
+//     if (!value) {
+//       alert('Please fill all fields');
+//       return;
+//     }
+//   }
