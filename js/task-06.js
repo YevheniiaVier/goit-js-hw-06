99def9ba1b19b2afd453a0e1d4499e1151d36835
YevheniiaVier <gotashk1983@gmail.com>
2022-10-12 21:39:01 +0300

    corrections

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,27 @@
 const inputRef = document.querySelector('#validation-input');
 
-inputRef.addEventListener('blur', onInputBlur);
+inputRef.addEventListener('focus', onInputFocus);
+inputRef.addEventListener('input', onInputChange);
 
-function onInputBlur(event) {
-  event.currentTarget.classList.add('valid') ||
-    event.currentTarget.classList.replace('invalid', 'valid');
-  if (event.currentTarget.value.length < event.currentTarget.dataset.length) {
+function onInputFocus() {
+  inputRef.style.outline = 'none';
+  event.currentTarget.classList.add('invalid');
+}
+
+function onInputChange(event) {
+  event.currentTarget.classList.add('invalid') ||
     event.currentTarget.classList.replace('valid', 'invalid');
+  if (event.currentTarget.value.length >= event.currentTarget.dataset.length) {
+    event.currentTarget.classList.replace('invalid', 'valid');
   }
 }
+
+// inputRef.addEventListener('blur', onInputBlur);
+
+// function onInputBlur(event) {
+//   event.currentTarget.classList.add('valid') ||
+//     event.currentTarget.classList.replace('invalid', 'valid');
+//   if (event.currentTarget.value.length < event.currentTarget.dataset.length) {
+//     event.currentTarget.classList.replace('valid', 'invalid');
+//   }
+// }
