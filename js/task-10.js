4a165d3b304b0e0aa1411129667f62bb24deb0ff
YevheniiaVier <gotashk1983@gmail.com>
2022-10-12 12:58:27 +0300

    cirrects task 10

--- js/task-10.js
+++ js/task-10.js
@@ -11,33 +11,26 @@ const boxesRef = document.querySelector('#boxes');
 
 buttonCreateRef.addEventListener('click', onCreateBtnClick);
 buttonDestroyRef.addEventListener('click', onDestroyBtnClick);
-inputNumberRef.addEventListener('input', onInputChange);
-
-let inputValue = inputNumberRef.value;
-const boxElements = [];
 
 function createBoxes(amount) {
+  const boxElements = [];
+
   for (let i = 0; i < amount; i += 1) {
     const boxEl = document.createElement('div');
     boxEl.style.width = `${30 + 10 * i}px`;
     boxEl.style.height = `${30 + 10 * i}px`;
     boxEl.style.backgroundColor = getRandomHexColor();
-    boxEl.classList.add('boxes__element');
-
     boxElements.push(boxEl);
-    console.log(boxEl);
   }
-  console.log(boxElements);
+
   return boxElements;
 }
 
 function onCreateBtnClick() {
-  createBoxes(inputValue);
-  boxesRef.append(...boxElements);
+  const inputValue = Number(inputNumberRef.value);
+  console.log(inputValue);
+  boxesRef.append(...createBoxes(inputValue));
 }
 function onDestroyBtnClick() {
   boxesRef.innerHTML = '';
-}
-function onInputChange() {
-  inputValue = inputNumberRef.value;
 }
